0ea26ff9553d4f3c0a9cd2628a58c29bb1031c6c
germandc <germancelis16@gmail.com>
2024-10-27 21:32:32 -0500

    Refactorizar estilos de tarjeta y diseño de página de inicio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primero: '#0b303c',
+				segundo: '#124b58',
+				tercero: '#1f727d',
+				cuarto: '#37a7ac',
+				quinto: '#5eebe2'
+			}
+		},
 	},
 	plugins: [],
 }
