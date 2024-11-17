2e4309fa0e4b5ddef10db9efa063f9ed650f4b5a
germandc <germancelis16@gmail.com>
2024-11-16 21:31:59 -0500

    Añadir soporte para obtener comparativas por ID y refactorizar la página de comparativas para mejorar la gestión de datos de vehículos

--- src/lib/getComparativas.js
+++ src/lib/getComparativas.js
@@ -10,3 +10,12 @@ export async function getComparativas() {
         })
     })
 }
+
+export async function getComparativaById(id) {
+    return consulta(`comparativas/1`)
+    .then( res => {
+        const { comparativa_id, vehiculo_id_1, vehiculo_id_2 } = res;
+    
+        return { comparativa_id, vehiculo_id_1, vehiculo_id_2 }
+    })
+}
