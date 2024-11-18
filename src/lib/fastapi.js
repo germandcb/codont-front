ebeaaf4064854fa62a5be6c5b37a774230fdd8bc
germandc <germancelis16@gmail.com>
2024-11-17 20:19:39 -0500

    Añadir funcionalidad para calcular y obtener huella de carbono; implementar métodos POST y consulta en FastAPI, y crear página para mostrar resultados

--- src/lib/fastapi.js
+++ src/lib/fastapi.js
@@ -3,3 +3,13 @@ const { FASTAPI_URL } = import.meta.env;
 export function consulta(url) {
     return fetch(`${FASTAPI_URL}/${url}`).then((res) => res.json());
 }
+
+export function post(url, body) {
+    return fetch(`http://127.0.0.1:8000/${url}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    }).then((res) => res.json());
+}
---
+++ src/lib/getHuellaCarbono.js
@@ -0,0 +1,10 @@
+import { consulta } from "./fastapi"; 
+
+export async function getHuellacarbonoById(id) {
+    return consulta(`calcular-co2/${id}`)
+    .then( res => {
+        const { tipo_combustible, huella_carbono_id, kilometros_recorridos, indice_eficiencia } = res;
+        
+        return { tipo_combustible, huella_carbono_id, kilometros_recorridos, indice_eficiencia }
+    })
+}
