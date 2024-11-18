166815ad5536aaeee7314c307677c8cc5237ec65
WeebB0i <smoreno003@outlook.com>
2024-11-17 21:43:55 -0500

    Texto bonito para el calculo de huella de carbono

--- src/lib/getHuellaCarbono.js
+++ src/lib/getHuellaCarbono.js
@@ -3,8 +3,8 @@ import { consulta } from "./fastapi";
 export async function getHuellacarbonoById(id) {
     return consulta(`calcular-co2/${id}`)
     .then( res => {
-        const { tipo_combustible, huella_carbono_id, kilometros_recorridos, indice_eficiencia } = res;
+        const { tipo_combustible, huella_carbono_id, kilometros_recorridos, indice_eficiencia, huella_carbono } = res;
         
-        return { tipo_combustible, huella_carbono_id, kilometros_recorridos, indice_eficiencia }
+        return { tipo_combustible, huella_carbono_id, kilometros_recorridos, indice_eficiencia, huella_carbono }
     })
 }
